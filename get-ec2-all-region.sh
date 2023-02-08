3864a3884f74d9f1a741fbb9631d37d287c3d2d1
samuraidays <1samuraiboy@gmail.com>
2023-02-08 16:50:57 +0800

    fix profile ec2

--- get-ec2-all-region.sh
+++ get-ec2-all-region.sh
@@ -1,7 +1,9 @@
-regions=(`aws-vault exec takashi@liveserver -- aws ec2 describe-regions --query Regions[*].RegionName --output text`)
+#!/bin/sh
+
+regions=(`aws-vault exec $1 -- aws ec2 describe-regions --query Regions[*].RegionName --output text`)
 
 for region in ${regions[@]}
 do echo "[${region}]"
-aws-vault exec takashi@liveserver -- aws ec2 describe-instances --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, State.Name, InstanceId, join(`, `, NetworkInterfaces[].Association.PublicIp), Tags[?Key==`Name`].Value|[0]]' --output text --region ${region}
+aws-vault exec $1 -- aws ec2 describe-instances --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, State.Name, InstanceId, join(`, `, NetworkInterfaces[].Association.PublicIp), Tags[?Key==`Name`].Value|[0]]' --output text --region ${region}
 echo "---------------------"
 done
