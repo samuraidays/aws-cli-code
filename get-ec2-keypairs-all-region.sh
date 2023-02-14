c91045b07338809d0a7cbf3d79977210ef962bc9
samuraidays <1samuraiboy@gmail.com>
2023-02-14 11:37:33 +0800

    fix keypair

--- get-ec2-keypairs-all-region.sh
+++ get-ec2-keypairs-all-region.sh
@@ -4,6 +4,10 @@ regions=(`aws-vault exec $1 -- aws ec2 describe-regions --query Regions[*].Regio
 
 for region in ${regions[@]}
 do echo "[${region}]"
-aws-vault exec $1 -- aws ec2 describe-key-pairs --query 'KeyPairs[*].[KeyPairId, KeyName]' --output text --region ${region}  | column -t
+aws-vault exec $1 -- aws ec2 describe-key-pairs --output table --region ${region} \
+    --query 'KeyPairs[*].{
+        ID: KeyPairId,
+        NAME: KeyName
+        }'
 echo "---------------------"
 done
